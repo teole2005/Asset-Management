7d56d2dfd023dd99ba355860c8e5d58842201366
teole2005 <01122005ct@gmail.com>
2026-03-10 13:44:24 +0700

    up

--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
 ﻿from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import os
 from app.api import assets
 
 app = FastAPI(title="Asset Management")
 
 app.include_router(assets.router)
+
+@app.get("/page", response_class=HTMLResponse)
+def get_page():
+    file_path = os.path.join(os.path.dirname(__file__), "static", "index.html")
+    with open(file_path, "r", encoding="utf-8") as f:
+        return f.read()
+
+@app.post("/api/action")
+def perform_action():
+    return {"message": "Action completed! Message sent from backend."}
 
 @app.get("/")
 def root():
