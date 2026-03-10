31d71a53777c3211c279c05ef790b0c5f54d500b
teole2005 <01122005ct@gmail.com>
2026-03-10 13:35:28 +0700

    update

---
+++ app/main.py
@@ -0,0 +1,10 @@
+﻿from fastapi import FastAPI
+from app.api import assets
+
+app = FastAPI(title="Asset Management")
+
+app.include_router(assets.router)
+
+@app.get("/")
+def root():
+    return {"status": "API is running"}
